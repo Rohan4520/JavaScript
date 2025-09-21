7a9953d4d48209b3cfe4c77a43bf5ac4c8f05e1f
Rohan yadav <145701101+Rohan4520@users.noreply.github.com>
2025-09-21 12:30:26 +0530

    Add files via upload

---
+++ class 12/function.js
@@ -0,0 +1,31 @@
+//practice for function , loop . conditional statement
+
+// function numCheck(num){
+//     for(let i=1; i<=num ; i++){
+//     if(i%2===0){
+//         console.log(i ,"even")
+//     }
+//     else(
+//         console.log(i , "odd")
+//     )
+// }
+// }
+// numCheck(50);
+
+
+// table printing
+// function printTable(n){
+//     for(let i=1 ; i<=10;i++){
+//         console.log(n*i)
+//     }
+// }
+
+// function printAllTable(){
+//     for(let i =1; i<=10; i++){
+//         printTable(i)
+//     }
+// }
+// printAllTable()
+
+//prime no
+
