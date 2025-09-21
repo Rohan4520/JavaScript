d8647fc652a063abe306f092413c87a4c4932c44
Rohan yadav <145701101+Rohan4520@users.noreply.github.com>
2025-09-21 12:30:06 +0530

    Add files via upload

---
+++ class 11/script.js
@@ -0,0 +1,100 @@
+
+// function greet(){
+//     console.log("Good Morning ")
+// }
+// greet();
+
+
+// function sum(x,y){
+//     console.log(x+y)
+// }
+// sum(5,6);
+
+
+// function sum(x){
+//   console.log(x)
+// }
+// sum(8);
+
+// function sum(){
+//   console.log(x)   // ReferenceError: x is not defined
+// }
+// sum(8);
+
+// function sum(x){
+//   console.log(x)   // undefine
+// }
+// sum();
+
+// function f1(a,b){
+//     return a**b
+// }
+// console.log(f1(4,2))   //16
+
+// function f2(a,b){
+//     return a * b
+// }
+// let mul = f2(5,5)
+// console.log(mul)
+
+//script scope  impure function
+// let  r  = 5;
+// function print(){
+//     return r++
+// }
+// console.log(print())
+// console.log(print())
+// console.log(print())
+// console.log(print())
+
+// console.log(hi());
+// let hi = function() {
+//     return "Hey!";
+// };
+
+
+// let sum = function addSum(){
+//     console.log("sum of no ")
+// }
+
+// sum();
+// function fname(){
+//     console.log("rohan")
+// }
+
+// fname();
+// function fulName(user){
+//     console.log( user()  + "yadav")
+// }
+// fulName(fname)
+
+//pattern
+//  for(let i = 1; i<=5 ; i++){
+//     console.log("*".repeat(i))
+//  }
+
+//by function 
+// function star(num){
+//     for(let i=1; i<=num;i++){
+//         let pattern = "";
+//         for(let j=1; j<=i;j++){
+//                 pattern += "*"
+//         }
+//         console.log(pattern)
+//     }
+// }
+// star(10)
+
+// let a = 10;
+
+// function test()Q {
+//   console.log(a);  Cannot access 'a' before initializatio TDZ
+//   let a = 5;
+// }
+
+// test();
+
+
+
+
+
